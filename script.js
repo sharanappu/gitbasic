3a10d308fd5bb7f74a21510738b07c0a1a5ce1a8
sharana basava <sharanpower581@gmail.com>
2024-02-10 11:39:47 +0530

    myinitialcode

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function speak() {
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-    alert('good morning');
-}
-function s() {
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
 
-    alert('good morning');
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
